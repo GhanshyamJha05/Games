a16d322bbb0331610f71304d3b42efff13cdd3d6
GhanshyamJha05 <168712839+GhanshyamJha05@users.noreply.github.com>
2024-10-04 23:49:01 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,49 @@
+import random
+
+while True:
+    choices=["rock","paper","scissors"]
+
+    computer=random.choice(choices)
+    player=None
+
+    while player not in choices:
+        player=input("rock, paper or scissors:").lower()
+
+        if player==computer:
+            print("computer:",computer)
+            print("player:",player)
+            print("Tie!")
+        elif player=="rock":
+            if computer=="paper":
+                print("computer:", computer)
+                print("player:", player)
+                print("you lose!")
+            if computer=="scissors":
+                print("computer:", computer)
+                print("player:", player)
+                print("you win!")
+        elif player=="paper":
+            if computer=="scissors":
+                print("computer:", computer)
+                print("player:", player)
+                print("you lose!")
+            if computer=="rock":
+                print("computer:", computer)
+                print("player:", player)
+                print("you win!")
+        elif player=="scissors":
+            if computer=="rock":
+                print("computer:", computer)
+                print("player:", player)
+                print("you lose!")
+            if computer=="paper":
+                print("computer:", computer)
+                print("player:", player)
+                print("you win!")
+
+    play_again=input("play again? (yes/no):").lower()
+
+    if play_again != "yes":
+        break
+
+print("Thanks for playing!")
